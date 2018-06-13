94f8fb67bf3191aee3c45a7850a2c64cb262ef95
Sagar <sagarthummar22@gmail.com>
2018-06-13 14:06:42 +0530

    updated few things

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,7 @@ let HtmlWebpackPlugin = require('html-webpack-plugin')
 mix.js('src/js/app.js', 'dist/js');
 
 mix.combine([
-    'src/vendor/dataTables/datatables.min.js',
+    //'src/vendor/dataTables/datatables.min.js',
     'src/vendor/jqueryui/jquery-ui.min.js',
     'src/vendor/sweetalert/sweetalert.min.js',
     'src/vendor/validate/jquery.validate.min.js'
@@ -22,13 +22,15 @@ mix.combine([
 
 mix.combine([
     'src/vendor/animate/animate.css',
-    'src/vendor/dataTables/datatables.min.css',
+    //'src/vendor/dataTables/datatables.min.css',
     'src/vendor/font-awesome/css/font-awesome.min.css',
     'src/vendor/jqueryui/jquery-ui.min.css',
     'src/vendor/src/vendor/sweetalert/sweetalert.css',
 ], 'dist/css/vendor.css');
 
 mix.sass('src/sass/app.scss', 'dist/css');
+
+mix.copyDirectory('src/vendor/font-awesome/fonts', 'dist/fonts');
 
 // mix.sourceMaps();
 mix.setPublicPath('dist');
@@ -45,7 +47,8 @@ mix.webpackConfig({
         new HtmlWebpackPlugin({template: "ejs-compiled-loader!./src/index.ejs", filename: "index.html", inject: false}),
         new HtmlWebpackPlugin({template: "ejs-compiled-loader!./src/about-us.ejs", filename: "about-us.html", inject: false}),
         new HtmlWebpackPlugin({template: "ejs-compiled-loader!./src/contact-us.ejs", filename: "contact-us.html", inject: false}),
-        new HtmlWebpackPlugin({template: "ejs-compiled-loader!./src/datatable-demo.ejs", filename: "datatable-demo.html", inject: false})
+        new HtmlWebpackPlugin({template: "ejs-compiled-loader!./src/datatable-demo.ejs", filename: "datatable-demo.html", inject: false}),
+        new HtmlWebpackPlugin({template: "ejs-compiled-loader!./src/font-awesome.ejs", filename: "font-awesome.html", inject: false})
     ],
     devServer: {
         //hot: true, inline: true, contentBase: __dirname
