50979dfcfaed5d2fae2154dcbd9a5a968440ba5b
Sagar <sagarthummar22@gmail.com>
2018-06-15 16:32:45 +0530

    Sweetalert demo done

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
-let mix = require('laravel-mix');
-let HtmlWebpackPlugin = require('html-webpack-plugin')
+let mix = require("laravel-mix");
+let HtmlWebpackPlugin = require("html-webpack-plugin");
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,44 +11,71 @@ let HtmlWebpackPlugin = require('html-webpack-plugin')
  |
  */
 
-mix.js('src/js/app.js', 'dist/js');
+mix.js("src/js/app.js", "dist/js");
 
-mix.combine([
-    'src/vendor/dataTables/datatables.min.js',
-    'src/vendor/jqueryui/jquery-ui.min.js',
-    'src/vendor/sweetalert/sweetalert.min.js',
-    'src/vendor/validate/jquery.validate.min.js'
-], 'dist/js/vendor.js');
+mix.combine(
+    [
 
-mix.combine([
-    'src/vendor/animate/animate.css',
-    'src/vendor/dataTables/datatables.min.css',
-    'src/vendor/font-awesome/css/font-awesome.min.css',
-    'src/vendor/jqueryui/jquery-ui.min.css',
-    'src/vendor/src/vendor/sweetalert/sweetalert.css',
-], 'dist/css/vendor.css');
+        "src/vendor/dataTables/datatables.min.js",
+        "src/vendor/jqueryui/jquery-ui.min.js",
+        "src/vendor/sweetalert/sweetalert.min.js",
+        "src/vendor/validate/jquery.validate.min.js"
+    ],
+    "dist/js/vendor.js"
+);
 
-mix.sass('src/sass/app.scss', 'dist/css');
+mix.combine(
+    [
+        "src/vendor/animate/animate.css",
+        "src/vendor/dataTables/datatables.min.css",
+        "src/vendor/font-awesome/css/font-awesome.min.css",
+        "src/vendor/jqueryui/jquery-ui.min.css",
+        //"src/vendor/sweetalert/sweetalert.css"
+    ],
+    "dist/css/vendor.css"
+);
 
-mix.copyDirectory('src/vendor/font-awesome/fonts', 'dist/fonts');
+mix.sass("src/sass/app.scss", "dist/css");
+
+mix.copyDirectory("src/vendor/font-awesome/fonts", "dist/fonts");
 
 // mix.sourceMaps();
-mix.setPublicPath('dist');
-mix.copyDirectory('src/img', 'dist/img');
+mix.setPublicPath("dist");
+mix.copyDirectory("src/img", "dist/img");
 
 mix.disableNotifications();
 
 mix.webpackConfig({
     output: {
         // path: path.join(__dirname, 'dist'),
-        publicPath: ''
+        publicPath: ""
     },
     plugins: [
-        new HtmlWebpackPlugin({template: "ejs-compiled-loader!./src/index.ejs", filename: "index.html", inject: false}),
-        new HtmlWebpackPlugin({template: "ejs-compiled-loader!./src/about-us.ejs", filename: "about-us.html", inject: false}),
-        new HtmlWebpackPlugin({template: "ejs-compiled-loader!./src/contact-us.ejs", filename: "contact-us.html", inject: false}),
-        new HtmlWebpackPlugin({template: "ejs-compiled-loader!./src/datatable-demo.ejs", filename: "datatable-demo.html", inject: false}),
-        new HtmlWebpackPlugin({template: "ejs-compiled-loader!./src/font-awesome.ejs", filename: "font-awesome.html", inject: false})
+        new HtmlWebpackPlugin({
+            template: "ejs-compiled-loader!./src/index.ejs",
+            filename: "index.html",
+            inject: false
+        }),
+        new HtmlWebpackPlugin({
+            template: "ejs-compiled-loader!./src/about-us.ejs",
+            filename: "about-us.html",
+            inject: false
+        }),
+        new HtmlWebpackPlugin({
+            template: "ejs-compiled-loader!./src/contact-us.ejs",
+            filename: "contact-us.html",
+            inject: false
+        }),
+        new HtmlWebpackPlugin({
+            template: "ejs-compiled-loader!./src/datatable-demo.ejs",
+            filename: "datatable-demo.html",
+            inject: false
+        }),
+        new HtmlWebpackPlugin({
+            template: "ejs-compiled-loader!./src/font-awesome.ejs",
+            filename: "font-awesome.html",
+            inject: false
+        })
     ],
     devServer: {
         //hot: true, inline: true, contentBase: __dirname
